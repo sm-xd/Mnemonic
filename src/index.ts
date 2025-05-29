3eeb85583c35f1bd3db82571cb712c167ae88008
Sky <126008711+sumitmishra01@users.noreply.github.com>
2025-05-30 03:23:17 +0530

    feat: add environment configuration for MongoDB URI, port, and JWT secret

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { app } from "./app";
-const port = "3000";
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
