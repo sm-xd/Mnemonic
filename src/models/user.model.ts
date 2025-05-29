336f40995e4645b838e864627a5e3917446a0643
Sky <126008711+sumitmishra01@users.noreply.github.com>
2025-05-29 23:44:01 +0530

    feat: define IUser interface for user model and update schema typing

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,8 +1,13 @@
 import mongoose, { Schema} from "mongoose";
 
-const userSchema : Schema = new Schema({
+export interface IUser extends Document {
+  username: string;
+  password: string;
+}
+
+const userSchema : Schema = new Schema<IUser>({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model<IUser>('User', userSchema);
